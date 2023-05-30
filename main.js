2161126363fed955115704f63b296a2bfdb13ee7
Bruno Cocchi <brunoo.cocchi@gmail.com>
2023-05-30 18:28:50 -0300

    Fiz o exercicio entregável da primeira aula de Js Coderhouse

---
+++ main.js
@@ -0,0 +1,12 @@
+let num1 = prompt('digite um número aqui.');
+let num2 = prompt('digite outro número aqui.');
+let soma = parseInt(num1) + parseInt(num2);
+
+
+if (soma)  {
+    alert('A soma dos dois números é: ' + soma)
+}
+else {
+    alert('Você não colocou números, recarregue a página e tente novamente.')
+}
+
